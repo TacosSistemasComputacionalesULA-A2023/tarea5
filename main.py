64fa874aa44c69100f942eab2361dc6114145042
Josmar Munoz <josmar.munoz@footballpools.com>
2023-02-15 23:28:21 -0400

    Updated changelog file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import gym
 import time
 import gym_taco_environments
 from agent import MonteCarlo
+import argparse
 
 
 def train(env, agent, episodes):
@@ -24,8 +25,18 @@ def play(env, agent):
         env.render()
         time.sleep(1)
 
+def define_parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--height", help="Matrix height", type=int)
+    parser.add_argument("-c", "--width", help="Matrix width", type=int)
+    parser.add_argument("-p", "--problem", help="Problem ID", type=str)
+    parser.add_argument("-f", "--file", help="Filepath", type=str)
+    parser.add_argument("-s", "--seed", help="Seed", type=int)
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
+    arguments = define_parse_arguments()
     env = gym.make("FrozenMaze-v0", render_mode="human", delay=0.5)
     agent = MonteCarlo(
         env.observation_space.n, env.action_space.n, gamma=0.9, epsilon=0.9
